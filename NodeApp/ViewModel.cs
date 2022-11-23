cd936a884fba41b317ed1098943d968b8bdd0d4b
Igor Popov <igor@igorpopov.io>
2022-11-23 21:07:12 +0200

    Add basic MVVM demo
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ NodeApp/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NodeApp
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
