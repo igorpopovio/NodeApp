ad1b828376e92794c7deaedd2c5793516593c05d
Igor Popov <igor@igorpopov.io>
2022-11-24 20:50:24 +0200

    Simplify code
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ NodeApp/Nodify/NodifyViewModels.cs
@@ -0,0 +1,36 @@
+﻿using NodeApp.Core;
+
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace NodeApp.Nodify
+{
+    class NodeViewModel : ViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public ObservableCollection<ConnectorViewModel> Input { get; set; } = new();
+        public ObservableCollection<ConnectorViewModel> Output { get; set; } = new();
+    }
+    class ConnectorViewModel : ViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public Point Anchor { get; set; }
+        public bool IsConnected { get; set; }
+    }
+
+    class ConnectionViewModel : ViewModel
+    {
+        public ConnectorViewModel? Source { get; set; }
+        public ConnectorViewModel? Target { get; set; }
+
+        public ConnectionViewModel(ConnectorViewModel source, ConnectorViewModel target)
+        {
+            Source = source;
+            Source.IsConnected = true;
+
+            Target = target;
+            Target.IsConnected = true;
+        }
+    }
+}
