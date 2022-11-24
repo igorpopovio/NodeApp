9b1030694b2774c381ad4b39678638dc0a4a269c
Igor Popov <igor@igorpopov.io>
2022-11-24 20:58:17 +0200

    Add a new connection
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- NodeApp/MainViewModel.cs
+++ NodeApp/MainViewModel.cs
@@ -18,18 +18,21 @@ namespace NodeApp
             node1.Input.Add(new ConnectorViewModel { Title = "In 1" });
             node1.Input.Add(new ConnectorViewModel { Title = "In 2" });
             node1.Output.Add(new ConnectorViewModel { Title = "Out 1" });
-
+            node1.Output.Add(new ConnectorViewModel { Title = "Out 2" });
             Nodes.Add(node1);
 
             var node2 = new NodeViewModel { Title = "Node 2", Location = new Point(500, 100) };
             node2.Input.Add(new ConnectorViewModel { Title = "In 1" });
+            node2.Input.Add(new ConnectorViewModel { Title = "In 2" });
             node2.Output.Add(new ConnectorViewModel { Title = "Out 1" });
             node2.Output.Add(new ConnectorViewModel { Title = "Out 2" });
-
             Nodes.Add(node2);
 
-            var connection = new ConnectionViewModel(source: node1.Output[0], target: node2.Input[0]);
-            Connections.Add(connection);
+            var connection1 = new ConnectionViewModel(source: node1.Output[0], target: node2.Input[0]);
+            Connections.Add(connection1);
+
+            var connection2 = new ConnectionViewModel(source: node1.Output[1], target: node2.Input[1]);
+            Connections.Add(connection2);
         }
     }
 }
